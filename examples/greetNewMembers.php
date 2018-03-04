5df56c0b0435fae923e352209ec9ef1d9ef28034
Charlotte Dunois <github@charuru.moe>
2018-03-04 18:08:11 +0100

    Update greetNewMembers.php

--- examples/greetNewMembers.php
+++ examples/greetNewMembers.php
@@ -15,7 +15,7 @@
 require_once(__DIR__.'/vendor/autoload.php');
 
 $loop = \React\EventLoop\Factory::create();
-$client = new \CharlotteDunois\Yasmin\Client(array(), $loop);
+$client = new \CharlotteDunois\Yasmin\Client($loop);
 
 $client->on('guildMemberAdd', function ($member) {
     try {
